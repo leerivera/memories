d655b9912967f948b58e9959d173fe1f4d7c4289
leerivera <leerivera80@gmail.com>
2021-02-22 15:05:32 -0500

    message

--- server/index.js
+++ server/index.js
@@ -13,12 +13,12 @@ const postRoutes = require('./routes/posts.js');
 
 const app = express();
 
-
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
 app.use('/posts', postRoutes);
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
 
 const CONNECTION_URL = 'mongodb+srv://nick:nick317718@cluster0.f4khk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
